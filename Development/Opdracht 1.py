68c69c2b4052cb988581ccfabd66d49b795feebf
Byron <Mr.boss2003@hotmail.com>
2020-12-04 21:51:17 +0100

    bijgewerkt

--- Development/Opdracht 1.py
+++ Development/Opdracht 1.py
@@ -5,9 +5,17 @@ class class1:
     _john = 2
     
     def tekst(self):
-        print("Hallo")
+        print("Hallo tim heeft zoveel punten in het begin: ",self._tim)
         
 
 werk = class1()
+werk2 = class1()
 
-print(werk._tim)
+werk.tekst()
+
+werk._tim = 3
+
+print("op werk heeft tim:",werk._tim)
+print("op dag 2 heeft tim:",werk2._tim)
+
+
