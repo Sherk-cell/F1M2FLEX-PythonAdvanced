614ba5b3d128614aa176d4c1517afa71ea0388cc
Byron <Mr.boss2003@hotmail.com>
2020-12-12 19:48:55 +0100

    Fresh start

--- Development/Opdracht 1.py
+++ Development/Opdracht 1.py
@@ -1,21 +1,11 @@
-
-
-class class1:
-    _tim = 1
-    _john = 2
-    
-    def tekst(self):
-        print("Hallo tim heeft zoveel punten in het begin: ",self._tim)
+class tim:
+    cijfer = 1
+    cijfer2 = 2   
+    def __init__(self, woord):
+       self.woord = woord
         
-
-werk = class1()
-werk2 = class1()
-
-werk.tekst()
-
-werk._tim = 3
-
-print("op werk heeft tim:",werk._tim)
-print("op dag 2 heeft tim:",werk2._tim)
-
-
+    def tekst(self):
+        print("hallo ik ben een", self.woord)
+        
+T = tim('leerling')
+T.tekst()
